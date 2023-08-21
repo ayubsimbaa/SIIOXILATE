75728fffd4369e4fef967a458cf5ed35dd5da556
Sazzad Hossain <tanimsazzad@gmail.com>
2023-08-22 02:44:21 +0600

    updated topic

--- src/components/Other/Topic.tsx
+++ src/components/Other/Topic.tsx
@@ -1,3 +1,4 @@
+import { Popover, PopoverContent, PopoverTrigger } from "components/ui/popover";
 import Image from "next/image";
 
 interface Props {
@@ -6,31 +7,64 @@ interface Props {
   variant: "withall" | "normal";
 }
 
-export default function Topic(props: Props) {
+function Topic2(props: Props) {
   return (
-    <div className="flex gap-4 max-w-[544px] items-center py-8 bg-gray-light px-14">
-      {/* Icon */}
-      <div>
-        <Image alt="icon" src={props.imageUrl} width={52} height={52} />
-      </div>
-      <div className="flex flex-col gap-3">
-        <p
-          className={`font-black text-lg  ${
-            props.variant === "normal" ? "text-black" : "text-blue-dark"
-          }`}
-        >
-          {props.title}
-        </p>
-        {props.variant === "withall" && (
-          <div className="flex gap-14">
-            <p className="font-semibold text-sm max-w-[248px]">
-              Many Insurance companies compete to provide services .
-            </p>
-            <Image alt="" src={"/arrow-up-right.svg"} width={39} height={39} />
+    <>
+      {props.variant === "normal" ? (
+        <Popover>
+          <PopoverTrigger>
+            <div className="flex gap-4 w-full items-center py-8 bg-gray-light mx-auto px-20">
+              {/* Icon */}
+              <div>
+                <Image alt="icon" src={props.imageUrl} width={52} height={52} />
+              </div>
+              <div className="flex flex-col gap-3">
+                <p
+                  className={`font-black text-lg ${
+                    props.variant === "normal" ? "text-black" : "text-blue-dark"
+                  }`}
+                >
+                  {props.title}
+                </p>
+              </div>
+              {/* Text */}
+            </div>
+          </PopoverTrigger>
+          <PopoverContent className="first-letter:capitalize bg-[#ffffff]">
+            no information available at this moment.
+          </PopoverContent>
+        </Popover>
+      ) : (
+        <>
+          <div className="flex gap-4 max-w-[544px] items-center py-8 bg-gray-light px-14">
+            {/* Icon */}
+            <div>
+              <Image alt="icon" src={props.imageUrl} width={52} height={52} />
+            </div>
+            <div className="flex flex-col gap-3">
+              <p className={`font-black text-lg text-blue-dark`}>
+                {props.title}
+              </p>
+              {props.variant === "withall" && (
+                <div className="flex gap-14">
+                  <p className="font-semibold text-sm max-w-[248px]">
+                    Many Insurance companies compete to provide services .
+                  </p>
+                  <Image
+                    alt=""
+                    src={"/arrow-up-right.svg"}
+                    width={39}
+                    height={39}
+                  />
+                </div>
+              )}
+            </div>
+            {/* Text */}
           </div>
-        )}
-      </div>
-      {/* Text */}
-    </div>
+        </>
+      )}
+    </>
   );
 }
+
+export default Topic2;
