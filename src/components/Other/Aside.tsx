48afb8ea199ef0f226fc5fbb5edd773ae5546550
Sazzad Hossain <tanimsazzad@gmail.com>
2023-08-22 10:23:35 +0600

    added aside

--- src/components/Other/Aside.tsx
+++ src/components/Other/Aside.tsx
@@ -1,6 +1,74 @@
+import Image from "next/image";
+import { Button } from "../ui/button";
 
 export default function Aside() {
-    return <div className="bg-gray-700 max-w-[425px] p-2 bg-gray-dark">aside</div>;
-  }
-  
-  
+  const options: OptionType[] = [
+    {
+      imageUrl: "/svg/home.svg",
+      title: "Home",
+    },
+    {
+      imageUrl: "/svg/attachment.svg",
+      title: "My Files",
+    },
+    {
+      imageUrl: "/svg/topic.svg",
+      title: "My Topics",
+    },
+    {
+      imageUrl: "/svg/mail.svg",
+      title: "My Emails",
+    },
+  ];
+
+  const options2: OptionType[] = [
+    {
+      imageUrl: "/svg/password.svg",
+      title: "Forgot Password",
+    },
+    {
+      imageUrl: "/svg/update.svg",
+      title: "Updates & FAQ",
+    },
+    {
+      imageUrl: "/svg/logout.svg",
+      title: "Log out",
+    },
+  ];
+  return (
+    <div className="bg-gray-700 max-w-[425px] p-5 bg-gray-dark grid">
+      <span className="font-extrabold text-3xl italic p-2">OXILATE</span>
+
+      <div className="flex flex-col">
+        <span>My Account</span>
+        <span>Welcome, Antonio Torrejon! 👋 </span>
+      </div>
+
+      <div className="grid gap-2 my-10 bg-gray-light/60 rounded-xl px-5">
+        {options.map((opt, index) => (
+          <Option {...opt} key={index} />
+        ))}
+      </div>
+
+      <div className="grid gap-2">
+        {options2.map((opt, index) => (
+          <Option {...opt} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+interface OptionType {
+  imageUrl: string;
+  title: string;
+}
+
+function Option(props: OptionType) {
+  return (
+    <Button variant={"with_icon"}>
+      <Image alt="" src={props.imageUrl} width={32} height={32} />
+      <span className="font-semibold">{props.title}</span>
+    </Button>
+  );
+}
