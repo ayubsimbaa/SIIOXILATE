8bc297895b9aca384e03bf59aca60b42786a26ab
Sazzad Hossain <tanimsazzad@gmail.com>
2023-08-23 18:29:35 +0600

    added title

--- src/app/competing_products/page.tsx
+++ src/app/competing_products/page.tsx
@@ -13,7 +13,11 @@ export default function Page() {
       {/* {competingProducts.map((item, index) => (
         <CompetingProduct {...item} key={index} />
       ))} */}
-      <DataTable data={competingProducts} columns={columns} />
+      <div className='my-5 bg-white p-4'>
+        <p className='ml-4 text-3xl font-bold'> Smart Insurance </p>
+        <p className='ml-4 text-3xl font-bold'>Competing Products!</p>
+        <DataTable data={competingProducts} columns={columns} />
+      </div>
     </div>
   )
 }
