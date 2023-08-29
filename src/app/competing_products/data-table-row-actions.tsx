0dd54fc3e825bf63a7c3f4ebd73cb78d27c5b906
Sazzad Hossain <tanimsazzad@gmail.com>
2023-08-29 12:33:53 +0600

    added handleShowIframe function

--- src/app/competing_products/data-table-row-actions.tsx
+++ src/app/competing_products/data-table-row-actions.tsx
@@ -10,7 +10,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@components/ui/dropdown-menu'
-import { useState } from 'react'
+import { useRef } from 'react'
 
 interface DataTableRowActionsProps<TData> {
   row: Row<TData>
@@ -19,12 +19,33 @@ interface DataTableRowActionsProps<TData> {
 export function DataTableRowActions<TData>({
   row,
 }: DataTableRowActionsProps<TData>) {
+  const iframeRef = useRef<HTMLIFrameElement | null>(null)
+
+  const handleShowIframe = () => {
+    const iframe = document.createElement('iframe')
+    iframe.src = 'https://next-js-front.vercel.app/'
+    iframe.title = 'Example Iframe'
+    iframe.className = 'w-full h-full'
+    iframe.style.position = 'fixed'
+    iframe.style.top = '0'
+    iframe.style.left = '0'
+    iframe.style.zIndex = '50'
+
+    document.body.appendChild(iframe)
+
+    // iframe.requestFullscreen().catch(err => {
+    //   alert(
+    //     `Error attempting to enable full-screen mode: ${err.message} (${err.name})`
+    //   )
+    // })
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
           variant='ghost'
-          className='flex h-8 w-8 p-0 data-[state=open]:bg-muted'
+          className='data-[state=open]:bg-muted flex h-8 w-8 p-0'
         >
           <DotsHorizontalIcon className='h-4 w-4' />
           <span className='sr-only'>Open menu</span>
@@ -32,38 +53,14 @@ export function DataTableRowActions<TData>({
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end' className='w-[160px]'>
         <DropdownMenuItem>
-          <Button variant={'ghost'}>Open Chat</Button>
+          <Button variant={'ghost'} onClick={handleShowIframe}>
+            Open Chat
+          </Button>
         </DropdownMenuItem>
         <DropdownMenuItem>
           <Button variant={'ghost'}>Delete</Button>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
-}
-
-const IframeComponent: React.FC = () => {
-  const [showIframe, setShowIframe] = useState(false)
-
-  return (
-    <div className='p-4'>
-      <button
-        className='bg-blue-500 rounded px-4 py-2 text-white'
-        onClick={() => setShowIframe(!showIframe)}
-      >
-        {showIframe ? 'Hide Iframe' : 'Show Iframe'}
-      </button>
-
-      {showIframe && (
-        <div className='mt-4'>
-          <iframe
-            src='https://www.example.com'
-            width='600'
-            height='400'
-            title='Example Iframe'
-          ></iframe>
-        </div>
-      )}
-    </div>
   )
 }
