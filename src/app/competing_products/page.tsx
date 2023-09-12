2539f3f09755a04cd7b9970c3a56145c71f74dd1
Sazzad Hossain <tanimsazzad@gmail.com>
2023-09-12 23:43:57 +0600

    removed showFrame logic

--- src/app/competing_products/page.tsx
+++ src/app/competing_products/page.tsx
@@ -1,36 +1,23 @@
-'use client'
-
+/**
+ * The above function is a React component that renders a page displaying a table of competing
+ * insurance products.
+ * @returns The Page component is being returned.
+ */
 import { competingProducts } from '@/src/fakeData/data'
-import { useCompetingStore } from '@/src/zustand_store/competingStore'
-import Iframe from 'react-iframe'
 import { columns } from './columns'
 import { DataTable } from './data-table'
 
 export default function Page() {
-  const showFrame = useCompetingStore(s => s.showFrame)
   return (
-    <div>
-      <div className='my-5 bg-grey p-4'>
-        {showFrame ? (
-          <Iframe
-            url='https://next-js-front.vercel.app/'
-            width='1220px'
-            height='634px'
-            id=''
-            className=''
-            display='block'
-            position='relative'
-          />
-        ) : (
-          <>
-            <p className='ml-4 text-3xl font-bold'> Smart Insurance </p>
-            <p className='ml-4 text-3xl font-bold'>Competing Products!</p>
-            <div className='px-4 py-2'>
-              <DataTable data={competingProducts} columns={columns} />
-            </div>
-          </>
-        )}
+    <div className='bg-white'>
+      <div className='bg-grey my-5 p-4'>
+        <p className='ml-4 text-3xl font-bold'> Smart Insurance </p>
+        <p className='ml-4 text-3xl font-bold'>Competing Products!</p>
+        <div className='px-4 py-2'>
+          <DataTable data={competingProducts} columns={columns} />
+        </div>
       </div>
-    </div>
-  )
+          
+    </div>
+  )
 }
