3977f117e8058b9fa7b49e9477fa4dcffe6d532e
Sazzad Hossain <tanimsazzad@gmail.com>
2023-09-19 12:12:18 +0600

    cleaned up code

--- src/app/document/layout.tsx
+++ src/app/document/layout.tsx
@@ -4,12 +4,8 @@ import Page from './page'
 
 export default function DashboardLayout() {
   return (
-    <div className='bg-[#ffffff] py-8'>
-      <div className='px-8 pb-10 pt-10'>
-        <Suspense fallback={<Loading />}>
-          <Page />
-        </Suspense>
-      </div>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <Page />
+    </Suspense>
   )
 }
