d0c447fda377a49652b33b675cf793a0600f2fae
Sazzad Hossain <tanimsazzad@gmail.com>
2023-09-20 23:12:34 +0600

    removed document to create dynamic document page

--- src/app/document/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import PdfViewer from '@/src/components/Other/PdfViewer'
-
-export default function Page() {
-  return <PdfViewer description='Sazzad' title='Tanim' usedAs='activeHistory' />
-}
