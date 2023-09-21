24b61c52834333ae68f4d568117b936e79347181
Sazzad Hossain <tanimsazzad@gmail.com>
2023-09-21 11:39:48 +0600

    fixed footer

--- src/components/Other/Footer.tsx
+++ src/components/Other/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className='absolute bottom-0 left-[25%] right-[25%] bg-gray-dark p-1 px-20 text-center text-[10px] font-medium'>
-      Copyright © 2023 Helsinski Insurance Limited. All Rights Reserved.
+     Copyright © 2023 Sii Oxilate. All Rights Reserved.
     </div>
   )
 }
