5cc9fce1b9a8dee12b071e747674ca83393766fc
Sazzad Hossain <tanimsazzad@gmail.com>
2023-09-21 12:29:19 +0600

    added title = document name

--- src/app/document/[id]/page.tsx
+++ src/app/document/[id]/page.tsx
@@ -3,10 +3,12 @@ import mockData from '@/src/app/my_files/mockData.json'
 import DocumentDetails from '@/src/components/Other/DocumentDetails'
 import PdfOutput from '@/src/components/Other/PdfOutput'
 import { ScrollArea } from '@/src/components/ui/scroll-area'
+import { useTopSectionHook } from '@/src/hooks/useTopSectionHook'
 import threeDot from '@svg/threeDot.svg'
 import Image from 'next/image'
 
 export default function Page({ params }: { params: { id: number } }) {
+  useTopSectionHook(mockData[params.id - 1].documentName)
   return (
     <div>
       <div className='flex flex-col bg-gray-500'>
