c78665d6d87082e99e57e942b0d46cfdd68d116d
Sazzad Hossain <tanimsazzad@gmail.com>
2023-09-21 11:33:41 +0600

    fixed scrolling issue

--- src/app/document/[id]/page.tsx
+++ src/app/document/[id]/page.tsx
@@ -2,7 +2,7 @@
 import mockData from '@/src/app/my_files/mockData.json'
 import DocumentDetails from '@/src/components/Other/DocumentDetails'
 import PdfOutput from '@/src/components/Other/PdfOutput'
-import { ScrollArea } from '@radix-ui/react-scroll-area'
+import { ScrollArea } from '@/src/components/ui/scroll-area'
 import threeDot from '@svg/threeDot.svg'
 import Image from 'next/image'
 
@@ -19,7 +19,7 @@ export default function Page({ params }: { params: { id: number } }) {
           <Image alt='' src={threeDot} className='mr-7' />
         </div>
 
-        <ScrollArea className='h-[300px] w-[350px] pl-8 pt-7 text-white'>
+        <ScrollArea className='h-[300px] w-full pl-8 pt-7 text-white'>
           {mockData[params.id - 1].details}
         </ScrollArea>
         <PdfOutput />
