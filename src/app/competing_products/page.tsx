dd614b776c6fba62151e272c34e0c3f2a542c2a0
Sazzad Hossain <tanimsazzad@gmail.com>
2023-09-21 12:27:59 +0600

    added title='competing products'

--- src/app/competing_products/page.tsx
+++ src/app/competing_products/page.tsx
@@ -3,11 +3,15 @@
  * insurance products.
  * @returns The Page component is being returned.
  */
+"use client"
 import { competingProducts } from '@/src/fakeData/data'
+import { useTopSectionHook } from '@/src/hooks/useTopSectionHook'
 import { columns } from './columns'
 import { DataTable } from './data-table'
 
 export default function Page() {
+  useTopSectionHook('Competing Products')
+
   return (
     <div className='bg-white'>
       <div className='bg-grey my-5 p-4'>
