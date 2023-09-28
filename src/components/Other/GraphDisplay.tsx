c58519c57db73f120d1adc2b8dba808655a1047b
Sazzad Hossain <tanimsazzad@gmail.com>
2023-09-28 12:36:29 +0600

    replaced cytoscape data

--- src/components/Other/GraphDisplay.tsx
+++ src/components/Other/GraphDisplay.tsx
@@ -1,56 +1,7 @@
-'use client'
-
-import { useState } from 'react'
+import { graphData } from '@/src/fakeData/data'
 import CytoscapeComponent from 'react-cytoscapejs'
 
 export default function GraphDisplay() {
-  const [width, setWith] = useState('100%')
-  const [height, setHeight] = useState('400px')
-
-  const [graphData, setGraphData] = useState({
-    nodes: [
-      { data: { id: '1', label: 'IP 1', type: 'ip' } },
-      { data: { id: '2', label: 'Coverage 1', type: 'Coverage' } },
-      { data: { id: '3', label: 'IP 2', type: 'ip' } },
-      { data: { id: '4', label: 'Coverage 2', type: 'Coverage' } },
-      { data: { id: '5', label: 'Coverage 3', type: 'Coverage' } },
-      { data: { id: '6', label: 'IP 3', type: 'ip' } },
-      { data: { id: '7', label: 'Coverage 5', type: 'Coverage' } },
-      { data: { id: '8', label: 'Coverage 6', type: 'Coverage' } },
-      { data: { id: '9', label: 'Coverage 7', type: 'Coverage' } },
-      { data: { id: '10', label: 'Coverage 8', type: 'Coverage' } },
-      { data: { id: '11', label: 'Coverage 9', type: 'Coverage' } },
-      { data: { id: '12', label: 'IP 3', type: 'ip' } },
-      { data: { id: '13', label: 'Coverage 10', type: 'Coverage' } },
-    ],
-    edges: [
-      {
-        data: { source: '1', target: '2', label: 'Node2' },
-      },
-      {
-        data: { source: '3', target: '4', label: 'Node4' },
-      },
-      {
-        data: { source: '3', target: '5', label: 'Node5' },
-      },
-      {
-        data: { source: '6', target: '5', label: ' 6 -> 5' },
-      },
-      {
-        data: { source: '6', target: '7', label: ' 6 -> 7' },
-      },
-      {
-        data: { source: '6', target: '8', label: ' 6 -> 8' },
-      },
-      {
-        data: { source: '6', target: '9', label: ' 6 -> 9' },
-      },
-      {
-        data: { source: '3', target: '13', label: ' 3 -> 13' },
-      },
-    ],
-  })
-
   const layout = {
     name: 'breadthfirst',
     fit: true,
@@ -64,7 +15,7 @@ export default function GraphDisplay() {
     nodeDimensionsIncludeLabels: false,
   }
 
-  const styleSheet = [
+  const style = [
     {
       selector: 'node',
       style: {
@@ -121,14 +72,14 @@ export default function GraphDisplay() {
       <CytoscapeComponent
         elements={CytoscapeComponent.normalizeElements(graphData)}
         // pan={{ x: 200, y: 200 }}
-        style={{ width: width, height: height }}
+        style={{ width: '100%', height: '400px' }}
         zoomingEnabled={true}
         maxZoom={3}
         minZoom={0.1}
         autounselectify={false}
         boxSelectionEnabled={true}
         layout={layout}
-        stylesheet={styleSheet}
+        stylesheet={style}
         // cy={cy => {
         //   myCyRef = cy;
 
@@ -141,7 +92,6 @@ export default function GraphDisplay() {
         //     console.log("TARGET TYPE", typeof node[0]);
         //   });
         // }}
-        abc={console.log('myCyRef', myCyRef)}
       />
     </div>
   )
--- src/fakeData/data.ts
+++ src/fakeData/data.ts
@@ -2,6 +2,49 @@ export interface productType {
   title: string
   location: string
 }
+export const graphData = {
+  nodes: [
+    { data: { id: '1', label: 'IP 1', type: 'ip' } },
+    { data: { id: '2', label: 'Coverage 1', type: 'Coverage' } },
+    { data: { id: '3', label: 'IP 2', type: 'ip' } },
+    { data: { id: '4', label: 'Coverage 2', type: 'Coverage' } },
+    { data: { id: '5', label: 'Coverage 3', type: 'Coverage' } },
+    { data: { id: '6', label: 'IP 3', type: 'ip' } },
+    { data: { id: '7', label: 'Coverage 5', type: 'Coverage' } },
+    { data: { id: '8', label: 'Coverage 6', type: 'Coverage' } },
+    { data: { id: '9', label: 'Coverage 7', type: 'Coverage' } },
+    { data: { id: '10', label: 'Coverage 8', type: 'Coverage' } },
+    { data: { id: '11', label: 'Coverage 9', type: 'Coverage' } },
+    { data: { id: '12', label: 'IP 3', type: 'ip' } },
+    { data: { id: '13', label: 'Coverage 10', type: 'Coverage' } },
+  ],
+  edges: [
+    {
+      data: { source: '1', target: '2', label: 'Node2' },
+    },
+    {
+      data: { source: '3', target: '4', label: 'Node4' },
+    },
+    {
+      data: { source: '3', target: '5', label: 'Node5' },
+    },
+    {
+      data: { source: '6', target: '5', label: ' 6 -> 5' },
+    },
+    {
+      data: { source: '6', target: '7', label: ' 6 -> 7' },
+    },
+    {
+      data: { source: '6', target: '8', label: ' 6 -> 8' },
+    },
+    {
+      data: { source: '6', target: '9', label: ' 6 -> 9' },
+    },
+    {
+      data: { source: '3', target: '13', label: ' 3 -> 13' },
+    },
+  ],
+}
 
 const competingProducts: productType[] = [
   {
